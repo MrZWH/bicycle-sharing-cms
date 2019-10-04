b6f0a97004e337750cd68a63381ec29a2adf3dcc
MrZWH <1875290692@qq.com>
2019-10-04 21:34:09 +0800

    搭建路由基础结构

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Icon } from 'antd';
-
+import {NavLink} from 'react-router-dom';
 import MenuConfig from '../../config/menuConfig';
 import './index.less';
 
@@ -22,7 +22,9 @@ export default class NavLeft extends Component {
         )
       }
 
-      return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+      return <Menu.Item key={item.key}>
+      <NavLink to={item.key}>{item.title}</NavLink>
+      </Menu.Item>
     }
     )
   }
